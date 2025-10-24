3cdf66bebc72b11c3b296050a52fd03987ac2c21
Aan_z7 <aannaruto191@gmail.com>
2025-10-24 13:05:15 +0700

    tes3

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -20,7 +20,40 @@ echo "luas lingkaran {$nama} ini adalah: {$rumus}";
         echo"udah ah cape";
     }
 }
+
+class KelilingLingkaran
+{
+    public const phi = 3.14;
+    public int $jari;
+    public function __construct($isijari = 1)
+    {
+        $this->jari = $isijari;
+    }
+
+    public function tampilKl($nama = 'ban')
+    {
+        $rumus2 = KelilingLingkaran::phi * (2 * $this->jari);
+        echo "Lingkaran " . ($nama) . " Hasil kelilingnya adalah: " . $rumus2 . "<br>";
+    }
+
+    public static function testing()
+    {
+        echo "</br>";
+        echo "Ini testing static keliling<br>";
+    }
+
+    public function __destruct()
+    {
+        echo "</br>";
+        echo "Udah capek keliling<br>";
+    }
+}
+
+$Lingkaran2 = new KelilingLingkaran(10);
+$Lingkaran2->tampilKl('ban');
+
 $lingkaran = new LuasLingkaran(10);
 $lingkaran->tampil('roda');
 echo'<br>';
 LuasLingkaran::testing();
+KelilingLingkaran::testing();
