c5ce884be416098e97feb7f76c98b08aba5b8b2c
Aan_z7 <aannaruto191@gmail.com>
2025-10-24 13:01:46 +0700

    tes2

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -4,10 +4,23 @@ class LuasLingkaran{
 
     public const phi = 3.14;
     public int $jari;
+public function __construct($isiJari = 1){
+   $this->jari = $isiJari;
 }
 
-$lingkaran = new LuasLingkaran();
-$lingkaran->jari = 10;
-$rumus = LuasLingkaran::phi * ($Lingkaran->jari * $Lingkaran->jari);
-echo "luas lingkaran dengan jari jari adalah". $rumus;
-//echo LuasLingkaran::phi;
+public function tampil($nama = 'ban'){
+    $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+echo "luas lingkaran {$nama} ini adalah: {$rumus}";
+    }
+    public static function testing() {
+        echo "ini testing static";
+    }
+    public function __destruct(){
+        echo"<br>";
+        echo"udah ah cape";
+    }
+}
+$lingkaran = new LuasLingkaran(10);
+$lingkaran->tampil('roda');
+echo'<br>';
+LuasLingkaran::testing();
